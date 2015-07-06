b4efe5eb62fefd4f6bcb9e29fadd4593fefa9ac8
Samuel Snopko <sas@gpm-agency.net>
2015-07-06 23:56:17 +0200

    restart project

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,16 +2,53 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
 
- 
-gulp.task('sass', function () {
-    gulp.src('./sass/styles.scss')
-        .pipe(sourcemaps.init())
-          .pipe(sass())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./css'));
+// prevent the errors stop watch
+var gulp_src = gulp.src;
+gulp.src = function() {
+  return gulp_src.apply(gulp, arguments)
+    .pipe(plumber(function(error) {
+      // Output an error message
+      gutil.log(gutil.colors.red('Error (' + error.plugin + '): ' + error.message));
+      // emit the end event, to properly end the task
+      this.emit('end');
+    }));
+};
+
+var showGreen = function(task) {
+  gutil.log(gutil.colors.green(':[' + task + ']: finshed without errors'));
+};
+
+// sass compiler
+gulp.task('sass', function(done) {
+  gulp.src('./sass/styles.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./css'));
+  showGreen('sass');
+  done();
 });
 
-gulp.task('watch', function () {
-   gulp.watch('./sass/**/*.scss', ['sass']);
+// watcher
+gulp.task('watch', function() {
+  watch('./sass/**/*.scss', function(vynil) {
+    gutil.log(gutil.colors.blue('[watch]: file edited : ' + vynil.relative));
+    gulp.start('sass');
+  });
 });
+
+// static server
+gulp.task('express', function() {
+  var express = require('express');
+  var app = express();
+  app.use(express.static(__dirname));
+  app.listen(4000);
+});
+
+gulp.task('default', ['express'], function() {});
